015f06cae264e9e66e30035598190d00814fea34
JavierJia <jianfeng.jia@gmail.com>
2014-02-11 17:22:49 -0800

    no return

--- hw6/24_1.rb
+++ hw6/24_1.rb
@@ -15,7 +15,7 @@ class TFQuarantine
             (v.is_a? Proc) ? v.call() : v
         end
 
-        print @funcs.each_with_object([lambda {return nil}]) { |f,v|
+        print @funcs.each_with_object([lambda {nil}]) { |f,v|
             v[0] = f.call(guard_callable(v[0]))
         }[0]
 
@@ -23,42 +23,42 @@ class TFQuarantine
 end
 
 def get_input args
-    return lambda { return ARGV[0] }
+    lambda { return ARGV[0] }
 end
 
 def read_file path_to_file
-    return lambda { return File.read(path_to_file) }
+    lambda { return File.read(path_to_file) }
 end
 
 def filter_chars allwords
-    return allwords.gsub (/[\W_]+/) , ' '
+    allwords.gsub (/[\W_]+/) , ' '
 end
 
 def normalize words
-    return words.downcase
+    words.downcase
 end
 
 def scan words
-    return words.split
+    words.split
 end
 
 def remove_stop_words words
-    return lambda { 
+    lambda { 
         stopwords = File.read('../stop_words.txt').downcase.split(',')
         return words.reject { |a| stopwords.include? a or a.length < 2 }
     }
 end
 
 def frequencies words
-    return words.each_with_object(Hash.new(0)) {|w,h| h[w] +=1; }
+    words.each_with_object(Hash.new(0)) {|w,h| h[w] +=1; }
 end
 
 def sort wf
-    return wf.sort_by {|a| -a[1]}
+    wf.sort_by {|a| -a[1]}
 end
 
 def print_top25 wf
-    return (wf.first(25).collect { |k, v| "#{k} - #{v}" }.join "\n") + "\n"
+    (wf.first(25).collect { |k, v| "#{k} - #{v}" }.join "\n") + "\n"
 end
 
 TFQuarantine.new().bind(method(:get_input))
